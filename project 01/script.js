94c6b9dea430a31214d1b0976ffce4ffbabc7621
Rana Ranveer Kumar Yadav <ranveer30654@gmail.com>
2024-10-18 19:12:13 +0530

    script.js

--- project 01/script.js
+++ project 01/script.js
@@ -55,3 +55,24 @@ var swiper = new Swiper(".coming-container", {
 });
 
 onclick="playVideo('Dhoom Machale Dhoom.mp4')"
+
+//services
+
+document.addEventListener("DOMContentLoaded", function() {
+    var acc = document.getElementsByClassName("accordion");
+    
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                panel.classList.remove('expanded');
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                panel.classList.add('expanded');
+            }
+        });
+    }
+});
